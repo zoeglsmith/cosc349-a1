fa805909ab7cb001dfa8075f8917c653970e9c77
smizo424 <smizo424@student.otago.ac.nz>
2023-09-28 19:46:04 +1300

    change

--- backend/server.js
+++ backend/server.js
@@ -74,7 +74,7 @@ app.use(cors(corsOptions));
 // });
 
 // Create a test route for POST requests
-app.post("/", (req, res) => {
+app.post("http://cosc349-a1-backend-url/test-post", (req, res) => {
   res.json({ message: "Test POST request successful" });
 });
 
